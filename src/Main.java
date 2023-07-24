76e418a64e868f41f2a54f9877ee538f48f90d10
Mike <109431533+1111334@users.noreply.github.com>
2023-07-24 16:31:49 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-import java.time.LocalDate;
 
+import java.time.LocalDate;
+import java.time.Year;
 
 public class Main {
     public static void main(String[] args) {
+        // Calcolo dei giorni mancanti alla fine del mese
+        LocalDate currentDate = LocalDate.now();
+        int endMonthMissing = currentDate.lengthOfMonth() - currentDate.getDayOfMonth();
+        System.out.println("Giorni mancanti alla fine del mese: " + endMonthMissing);
 
-        LocalDate localDate = LocalDate.now();
-        System.out.println(localDate);
-
-        int endMonthMissing =  localDate.lengthOfMonth() - localDate.getDayOfMonth();
-        int endYearMissing = localDate.lengthOfYear() - localDate.getDayOfYear();
-
-        System.out.println("Days remaining at the end of the month " + endMonthMissing);
-        System.out.println("days remaining at the end of the year" + endYearMissing);
+        // Calcolo dei giorni mancanti alla fine dell'anno
+        Year currentYear = Year.of(currentDate.getYear());
+        int endYearMissing = currentDate.lengthOfYear() - currentDate.getDayOfYear();
+        System.out.println("Giorni mancanti alla fine dell'anno: " + endYearMissing);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
